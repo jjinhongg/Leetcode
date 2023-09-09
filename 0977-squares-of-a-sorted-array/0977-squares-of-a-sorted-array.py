c2ba2d5eabf4fa602fe4dc8ea29badc22013a1d0
Jin Hong YONG <34515006+jjinhongg@users.noreply.github.com>
2023-09-09 11:51:38 +1000

    Time: 218 ms (41.83%), Space: 18.7 MB (42.37%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,18 +1,18 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        result = [0] * len(nums)
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            # compare the absolute values of the elements at left and right pointers
-            if abs(nums[left]) > abs(nums[right]):
-                # insert the larger squared value at the end of result
-                result[right - left] = nums[left] ** 2
-                left += 1
+        answer = collections.deque()
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            leftAbs, rightAbs = abs(nums[l]), abs(nums[r])
+            if leftAbs > rightAbs:
+                answer.appendleft(leftAbs**2)
+                l += 1
+                
             else:
-                # insert the larger squared value at the end of result
-                result[right - left] = nums[right] ** 2
-                right -= 1
-
-        return result
+                answer.appendleft(rightAbs**2)
+                r -= 1
+                
+        return list(answer)
+        
 
