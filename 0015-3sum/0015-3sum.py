8e2d2b2c5cdc2f25d31aad33f5625566c90ac537
Jin Hong YONG <34515006+jjinhongg@users.noreply.github.com>
2023-09-11 20:46:55 +1000

    Time: 656 ms (96.45%), Space: 20.9 MB (17.18%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,46 @@
-from typing import List
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # sort the array in ascending order
-        nums.sort()
-        # initialize an empty list to store the triplets
-        result = []
-        # loop through the array from left to right
-        for i in range(len(nums) - 2):
-            # skip duplicates
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
 
-            left = i + 1
-            right = len(nums) - 1
+        res = set()
 
-            while left < right:
-                sum = nums[i] + nums[left] + nums[right]
-                if sum == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    right -= 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif sum < 0:
-                    left += 1
-                else:
-                    right -= 1
+        #1. Split nums into three lists: negative numbers, positive numbers, and zeros
+        n, p, z = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0: 
+                n.append(num)
+            else:
+                z.append(num)
 
-        # return the result list
-        return result
+        #2. Create a separate set for negatives and positives for O(1) look-up times
+        N, P = set(n), set(p)
 
+        #3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
+        #   i.e. (-3, 0, 3) = 0
+        if z:
+            for num in P:
+                if -1*num in N:
+                    res.add((-1*num, 0, num))
+
+        #3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
+        if len(z) >= 3:
+            res.add((0,0,0))
+
+        #4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
+        #   exists in the positive number set
+        for i in range(len(n)):
+            for j in range(i+1,len(n)):
+                target = -1*(n[i]+n[j])
+                if target in P:
+                    res.add(tuple(sorted([n[i],n[j],target])))
+
+        #5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
+        #   exists in the negative number set
+        for i in range(len(p)):
+            for j in range(i+1,len(p)):
+                target = -1*(p[i]+p[j])
+                if target in N:
+                    res.add(tuple(sorted([p[i],p[j],target])))
+
+        return res
